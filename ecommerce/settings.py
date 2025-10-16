6d646145cbfe162cd296016c3cee17ffcda8e727
Fabio souza <souzadgrafico@gmail.com>
2025-10-16 18:54:19 -0300

    settings-database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'postgresql://postgres:ldIUHzpBJCdzqGigyWDjYgKrrpxKlGIc@shinkansen.proxy.rlwy.net:46983/railway'
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
