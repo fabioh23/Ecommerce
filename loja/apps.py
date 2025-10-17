592fd1ec003b86e8a8390e021e1544239c861e6e
Fabio souza <souzadgrafico@gmail.com>
2025-10-17 09:02:06 -0300

    outra tentativa

--- loja/apps.py
+++ loja/apps.py
@@ -5,15 +5,15 @@ class LojaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'loja'
 
-    def ready(self):
-        from django.contrib.auth.models import User
-        from .models import Cliente
-        import os
-
-        email = os.getenv('EMAIL_ADMIN')
-        senha = os.getenv('SENHA_ADMIN')
-
-        usuarios = User.objects.filter(email=email)
-        if not usuarios:
-            User.objects.create_superuser(username=email, email=email, password=senha, is_active=True, is_staff=True)
-            Cliente.objects.create(email=email, usuario__id=id)
+    # def ready(self):
+    #     from django.contrib.auth.models import User
+    #     from .models import Cliente
+    #     import os
+    #
+    #     email = os.getenv('EMAIL_ADMIN')
+    #     senha = os.getenv('SENHA_ADMIN')
+    #
+    #     usuarios = User.objects.filter(email=email)
+    #     if not usuarios:
+    #         User.objects.create_superuser(username=email, email=email, password=senha, is_active=True, is_staff=True)
+    #         Cliente.objects.create(email=email, usuario__id=id)
