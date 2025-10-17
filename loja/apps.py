67cc8b40ff05effe26312b8baab2b27463103cb7
Fabio souza <souzadgrafico@gmail.com>
2025-10-16 21:10:01 -0300

    superuser

--- loja/apps.py
+++ loja/apps.py
@@ -5,13 +5,13 @@ class LojaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'loja'
 
-    def ready(self):
-        from django.contrib.auth.models import User
-        import os
-
-        email = os.getenv('EMAIL_ADMIN')
-        senha = os.getenv('SENHA_ADMIN')
-
-        usuarios = User.objects.filter(email=email)
-        if not usuarios:
-            User.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
+    # def ready(self):
+    #     from django.contrib.auth.models import User
+    #     import os
+    #
+    #     email = os.getenv('EMAIL_ADMIN')
+    #     senha = os.getenv('SENHA_ADMIN')
+    #
+    #     usuarios = User.objects.filter(email=email)
+    #     if not usuarios:
+    #         User.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
