bf3aa76c070de9c68a8119b59c91f62e5e20cdc0
Fabio souza <souzadgrafico@gmail.com>
2025-10-17 10:05:12 -0300

    novamente4

--- loja/apps.py
+++ loja/apps.py
@@ -18,9 +18,11 @@ class LojaConfig(AppConfig):
         if not usuarios:
             User.objects.create_superuser(username=email, email=email, password=senha, is_active=True, is_staff=True)
 
-        User = get_user_model()
-        user_instance = User.objects.get(pk=user_id)
-        cliente.user = user_instance
         super_user = User.objects.get(email=email, is_staff=True, is_superuser=True)
+        super_user_id = super_user.pk
+
+        User = get_user_model()
+        user_instance = User.objects.get(pk=super_user_id)
+        cliente.user = user_instance
         if super_user:
             Cliente.objects.create(email=email, usuario=cliente.user)
