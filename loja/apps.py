4ca332aac529f273ef59c4fc5aa6bb5ca392786b
Fabio souza <souzadgrafico@gmail.com>
2025-10-17 09:59:25 -0300

    novamente3

--- loja/apps.py
+++ loja/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 
 
 class LojaConfig(AppConfig):
@@ -17,7 +18,9 @@ class LojaConfig(AppConfig):
         if not usuarios:
             User.objects.create_superuser(username=email, email=email, password=senha, is_active=True, is_staff=True)
 
+        User = get_user_model()
+        user_instance = User.objects.get(pk=user_id)
+        cliente.user = user_instance
         super_user = User.objects.get(email=email, is_staff=True, is_superuser=True)
-        # super_user_id = super_user.pk
         if super_user:
-            Cliente.objects.create(email=email, usuario=1)
+            Cliente.objects.create(email=email, usuario=cliente.user)
